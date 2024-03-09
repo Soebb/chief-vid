4456cf39766f895cd50d61b4a7e0714e1280648d
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 18:39:15 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,8 @@ cd /app
 docker build -t boehmls/chief-video:latest .
 docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
 sudo dockerd --iptables=false
+sudo systemctl stop docker
+sudo firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
+sudo systemctl start docker
+# in most cases existing networks will lose mapping
+docker network prune
