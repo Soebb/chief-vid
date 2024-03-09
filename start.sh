31696dfd17f0a387ffe68c1b23ca51d0939bda82
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 22:24:47 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 cd /app
-sudo chmod 755 -R .
+
 sudo docker-compose up
-docker info
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo reboot
+
+#docker info
 #sudo docker-compose up
 #docker build -t boehmls/chief-video:latest .
 #docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
 #dockerd --iptables=false
 #firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
 #firewall-cmd --reload
-service dbus start
+#service dbus start
 #service docker start
 #sudo service dbus start
-#sudo service docker start
+sudo service docker start
 #sudo rm -rf /var/lib/docker/network
 #docker ps -a | cut -d' ' -f 1 | xargs -n 1 echo docker rm -f
