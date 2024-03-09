d058fad8460b2a4b59c0c93241fc1b42b8dea25d
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 22:21:48 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 cd /app
 sudo chmod 755 -R .
 sudo docker-compose up
+docker info
 #sudo docker-compose up
 #docker build -t boehmls/chief-video:latest .
 #docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
