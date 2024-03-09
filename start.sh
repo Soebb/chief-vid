cba2e650fc9085db5e78818c6e8489fea2626bbb
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 18:09:39 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 cd /app
 docker build -t boehmls/chief-video:latest .
 docker run --rm -it -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
+sudo service docker start
