17ae8c457b0174a0082206c86c458d96a5987d81
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 21:15:06 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 cd /app
-docker build -t boehmls/chief-video:latest .
-docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
-dockerd --iptables=false
-firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
-firewall-cmd --reload
+docker-compose up
+#docker build -t boehmls/chief-video:latest .
+#docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
+#dockerd --iptables=false
+#firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
+#firewall-cmd --reload
 service dbus start
 #service docker start
 #sudo service dbus start
