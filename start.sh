b89191a54e6817bedc1ebd675406ab9394ae0af6
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 22:21:25 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 cd /app
-sudo groupadd docker
-sudo usermod -aG docker $USER
-# Log out and log back in again to apply the groups
-groups
-docker-compose up
+sudo chmod 755 -R .
+sudo docker-compose up
 #sudo docker-compose up
 #docker build -t boehmls/chief-video:latest .
 #docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
