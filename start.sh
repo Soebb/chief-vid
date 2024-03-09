b563442a3c5cbfca9cde4f507943ddd17405ee2c
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 19:06:38 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ docker build -t boehmls/chief-video:latest .
 docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
 sudo dockerd --iptables=false
 sudo firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
-service dbus start
+sudo service dbus start
 sudo service docker start
