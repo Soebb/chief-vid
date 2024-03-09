fd8a71572b7bd521a75aa82e80d7378ef49a9461
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 18:56:39 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,3 @@ docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_U
 sudo dockerd --iptables=false
 sudo firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
 sudo service docker start
-# in most cases existing networks will lose mapping
-docker network prune
