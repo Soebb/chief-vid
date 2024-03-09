038ca67a99b6772b7cb0035e805d3654f85d77d9
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 18:27:33 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd /app
 docker build -t boehmls/chief-video:latest .
 docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
-sudo dockerd
+sudo dockerd --iptables=false
