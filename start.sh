c1d0e09d9dcd8f1d559d4e08689cc5043a8e8391
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-15 18:24:29 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ cd /apps
 docker-run-cmd Dockerfile
 #sudo service docker restart
 sudo dockerd --iptables=false
+sudo firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
+sudo firewall-cmd --reload
