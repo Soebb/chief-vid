6a9b726bb2bb62add4d3629ae76f86c7915caced
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-15 16:05:27 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
-cd /apps/app
-gunicorn --workers=3 -b :5000 --log-level debug app:app
-
-#docker run -v /var/run/docker.sock:/var/run/docker.sock
-#sudo chown root:docker /var/run/docker.sock
-#sudo docker-compose build
-
-#docker info
-#sudo docker-compose up
-#docker build -t boehmls/chief-video:latest .
-#docker run --privileged --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
-#dockerd --iptables=false
-#firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
-#firewall-cmd --reload
-#service dbus start
-#service docker start
-#sudo service dbus start
-#sudo service docker start
-#sudo rm -rf /var/lib/docker/network
-#docker ps -a | cut -d' ' -f 1 | xargs -n 1 echo docker rm -f
+cd /apps
+run-docker-cmd Dockerfile
