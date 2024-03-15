4d4977455cacd5e71ee85d67931b30419fde4cfa
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-15 18:56:05 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,8 @@ docker-run-cmd Dockerfile
 sudo dockerd --iptables=false
 sudo firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
 sudo firewall-cmd --reload
-sudo service dbus start
-sudo service docker start
+sudo grep -r dbus /etc/systemd/system/*
+/etc/systemd/system/dbus-org.freedesktop.resolve1.service:Alias=dbus-org.freedesktop.resolve1.service
+/etc/systemd/system/dbus-org.freedesktop.timesync1.service:Alias=dbus-org.freedesktop.timesync1.service
+#sudo service dbus start
+#sudo service docker start
