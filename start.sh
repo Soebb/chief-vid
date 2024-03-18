25a3a0a5100d9155c6b54a81152387bc3d2adba7
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-18 22:24:11 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cd /apps
-id
-#docker build -t boehmls/chief-video:latest .
-#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
+docker build -t boehmls/chief-video:latest .
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
