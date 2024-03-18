30eaef76aa8bef3635d34f710fdfe407c65ea292
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-18 18:23:40 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 cd /apps
-service docker stop
-cd /var/run/docker/libcontainerd
-rm -rf containerd/*
-rm -f docker-containerd.pid
-service docker start
 docker build -t boehmls/chief-video:latest .
 docker run --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
