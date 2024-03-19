b5e086b5b9321661be06339dd830e8dfe5e69292
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-19 03:45:47 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 cd /apps
-docker-run-cmd Dockerfile
-#sudo service docker restart
-sudo dockerd --iptables=false
-sudo firewall-cmd --zone=FedoraWorkstation --remove-interface=docker0 --permanent
-sudo firewall-cmd --reload
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
-sudo service start dbus.socket
+podman-compose up
