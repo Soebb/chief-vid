b4cb7814a89f919191faffa2a6882d79bdc05e84
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-20 19:24:00 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ start-docker.sh
 # Your commands go here
 cd /apps
 docker service restart
-docker build . boehmls/chief-video:latest
+docker build -t boehmls/chief-video:latest .
 docker run --privileged ubuntu-dind docker run --rm -v /run/user/1000/docker.sock:/run/user/1000/docker.sock -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
 #docker run --privileged ubuntu-dind docker compose up
 docker service restart
