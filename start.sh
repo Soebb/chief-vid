485d6b8db414e7602e160f1ca94c2b9d3e11acb4
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-26 02:18:56 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /apps
 docker run -d --privileged --name mydind docker:dind
-
+docker exec -it mydind docker date
 docker build -t boehmls/chief-video:latest .
 docker run --rm -v /app/videos:/app/videos -p 5000:5000 -e "VIDEO_URL=/app/videos" boehmls/chief-video:latest
 
